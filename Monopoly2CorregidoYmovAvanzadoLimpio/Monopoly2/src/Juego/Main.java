fc3f36ba040d668b32514c8caac2a1199d609f43
ivan el guapo <151021083+IvanElGuapo@users.noreply.github.com>
2023-12-04 11:37:58 +0100

    Add files via upload

---
+++ Monopoly2CorregidoYmovAvanzadoLimpio/Monopoly2/src/Juego/Main.java
@@ -0,0 +1,27 @@
+package Juego;
+//MONOPOLY V2
+//Mateito, Cese y Mencía
+
+import java.beans.PersistenceDelegate;
+import java.util.Scanner;
+import java.util.*;
+import Procesos.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡--Empieza el juego!--");
+        //INICIALIZACIÓN
+        Juego mono = new Juego();
+
+        //DAR DE ALTA A LOS JUGADORES
+        mono.turnoInicial();
+
+        boolean continuar = true;
+        while (continuar) {
+            continuar = mono.menuAccion(false);
+        }
+
+        //FIN DE JUEGO
+        System.out.println("Muchas gracias por jugar, ¡hasta la próxima!");
+    }
+}
